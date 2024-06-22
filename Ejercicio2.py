2bec82f4db4d6010dbae84f1c62d731db375f8c3
Luissoro <luissopenar@gmail.com>
2024-06-22 19:11:12 +0200

    Empezando en Phyton
    Soy un chaval que ha terminado el primer curso de grado medio de SMR y se ha comprado un libro de python práctico para empezar, antes de hacer una base tienes que saber que materiales vas a tener que utilizar

---
+++ Ejercicio2.py
@@ -0,0 +1,21 @@
+#Usar print("x","y","z")
+print ("Hola","estoy","usando","comas","en","vez","de","espacios")
+print ("Tambien puedo poner números")
+print ("1","2","3","4")
+print (0,1,2,3,4,5,6,7,8,9)
+print (" ")
+print("Hola Python", 2024)
+print("hola mundo 2024")
+
+texto="Hola año"
+año=2024
+print(texto,año)
+
+print(1,2,3,4,)
+print(1,2,3,4, sep=',')
+print(1,2,3,4,)
+print(1,2,3,4, sep=',', end='.')
+
+print("\nPrimera línea: \"Texto entre comillas\"\nSegunda línea: \'\x24\'")
+
+print(" ")
---
+++ Ejercicio3.py
@@ -0,0 +1,14 @@
+#Descomprimido
+print("Introduzca su nombre:")
+nombre=input()
+print("Introduzca su primer apellido:")
+primerapellido=input()
+print("Introduzca su segundo apellido:")
+segundoapellido=input()
+print ("Gracias por sus datos, se los digo por si hay algo mal")
+print(primerapellido,segundoapellido,nombre, end='.')
+
+#Comprimido
+nombre=input("Introduzca su nombre:")
+apellidos=input("Introduzca sus apellidos:")
+print("Bienvenido",nombre,apellidos)
